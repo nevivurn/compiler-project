9a441a9be837854eca2ff3874f6646325d4818b9
Yongun Seong <nevivurn@nevi.dev>
2023-09-26 20:50:33 +0900

    feat: add phase 1 report

---
+++ reports/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          name = "snucc-reports";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pandoc texlive.combined.scheme-medium ];
+
+          buildPhase = ''
+            runHook preBuild
+            for f in *.md; do
+              pandoc "$f" -o "''${f%.md}.pdf"
+            done
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 -t $out *.pdf
+            runHook postInstall
+          '';
+        };
+      }
+    );
+}
